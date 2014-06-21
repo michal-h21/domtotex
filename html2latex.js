8ba0a8fe1500b66e6725b2a2c4eab040c6ae3918
Michal Hoftich <michal.h21@gmail.com>
2014-06-21 21:49:18 +0200

    Module for phantom.js and slimer.js

---
+++ html2latex.js
@@ -0,0 +1,39 @@
+var system = require("system");
+var page   = require("webpage").create(), address, base;
+if(system.args.length === 1) {
+				console.log("Usage html2latex url");
+				phantom.exit();
+} else {
+address = system.args[1];
+base = "body"; 
+if (system.args[2]){
+  base = system.args[2];
+}
+page.onConsoleMessage = function(msg){
+  phantom.outputEncoding = "utf-8";
+	console.log(msg);
+};
+console.log("% saved page: "+address);
+var traverse = "traverser.js";
+var mustache = "mustache.js";
+page.open(address, function(st){
+  var injst = page.injectJs(traverse);//, function(){
+	page.injectJs(mustache);
+    page.evaluate(function(){
+      var ti = document.querySelector("title")
+      var title = getText(ti);
+			//var title=getText;
+			console.log("\\documentlass{scrartcl}");
+			console.log("\\usepackage{fontspec}");
+			latexCommand("title",title);
+			console.log("\\begin{document}");
+			var body = getText(document.querySelector("body"));
+			if(body.length > 0) {
+			   console.log(body);
+			}
+		});
+    phantom.exit();
+	//});
+  //phantom.exit();
+});
+}
